5eb1b9263a3208bd9677ada4aee0ab3c5bc3bec2
Ivy Xu <ivyxu@vouch.com>
2015-10-24 16:48:08 -0700

    url change

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^big_teacher/', include('big_teacher.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('big_teacher.urls')),
 ]
