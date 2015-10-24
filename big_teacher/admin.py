3a4575c877340d746b40b5888d403ea5be27bcfd
Ivy Xu <ivyxu@vouch.com>
2015-10-24 14:58:35 -0700

    PLaying around with admin page

--- big_teacher/admin.py
+++ big_teacher/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
+from django.contrib import admin
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['answer_text', 'question_text']
+
 admin.site.register(Question)
